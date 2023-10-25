2b164029fce6c3cc1fbdb104b3f06cd0b36d9c5a
Joachim <jschalk@users.noreply.github.com>
2023-10-25 13:35:30 -0700

    #33 testing refactor

--- src/culture/test_culture_/test_culture_.py
+++ src/culture/test_culture_/test_culture_.py
@@ -229,3 +229,18 @@ def test_cultureunit_shop_CorrectlyReturnsObj(env_dir_setup_cleanup):
     assert os_path.exists(culture_dir)
     assert x_culture._bank_db != None
     assert x_culture._manager_name == sue_text
+
+
+def test_cultureunit_set_manager_name_CorrectsSetsData(env_dir_setup_cleanup):
+    # GIVEN
+    x_handle = get_temp_env_handle()
+    x_culture = cultureunit_shop(handle=x_handle, cultures_dir=get_test_cultures_dir())
+    assert x_culture.handle == x_handle
+    assert x_culture._manager_name is None
+
+    # WHEN
+    zuo_text = "Zuo"
+    x_culture.set_manager_name(zuo_text)
+
+    # THEN
+    assert x_culture._manager_name == zuo_text
