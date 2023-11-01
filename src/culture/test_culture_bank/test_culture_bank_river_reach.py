be9bc2e9bee02c2accc05bc300b7644d8bcac7e9
Joachim <jschalk@users.noreply.github.com>
2023-11-01 16:49:10 -0700

    #40 make query tester faster

--- src/culture/test_culture_bank/test_culture_bank_river_reach.py
+++ src/culture/test_culture_bank/test_culture_bank_river_reach.py
@@ -1,4 +1,4 @@
-from src.culture.culture import cultureunit_shop
+from src.culture.culture import cultureunit_shop, get_river_ledger_unit
 from src.culture.examples.culture_env_kit import (
     get_temp_env_handle,
     get_test_cultures_dir,
@@ -12,23 +12,28 @@ from src.culture.bank_sqlstr import (
 )
 
 
-def test_culture_clear_river_score_CorrectlyEmptysTable(env_dir_setup_cleanup):
-    # GIVEN
-    ex7_text = "ex7"
-    x_culture = _delete_and_set_ex6(x_handle=ex7_text)
-    sal_text = "sal"
-    reach_sqlstr = get_river_block_reach_base_sqlstr(sal_text)
+# def test_culture_river_block_reach_base_sqlstr_CorrectlySelectsDataSet(
+#     env_dir_setup_cleanup,
+# ):
+#     # GIVEN
+#     ex7_text = "ex7"
+#     x_culture = _delete_and_set_ex6(x_handle=ex7_text)
+#     sal_text = "sal"
+#     # x_culture = _delete_and_set_ex6()
+#     # # x_culture.set_manager_name(sal_text)
+#     # ex6_text = "ex6"
+#     # x_culture = cultureunit_shop(handle=ex6_text, cultures_dir=get_test_cultures_dir())
+#     # x_culture.set_manager_name(sal_text)
+#     # x_culture.set_credit_flow_for_agenda(sal_text, max_blocks_count=100)
 
-    # WHEN
-    count_reach_rows_str = f"""
-SELECT COUNT(*)
-FROM (
-    {reach_sqlstr}
-) x
-"""
-    count_reach_rows_num = get_single_result(
-        x_culture.get_bank_conn(), count_reach_rows_str
-    )
+#     # WHEN
+#     reach_sqlstr = get_river_block_reach_base_sqlstr(sal_text)
+#     reach_count_sqlstr = f"""SELECT COUNT(*) FROM ({reach_sqlstr}) x;"""
+#     reach_rows_num = get_single_result(x_culture.get_bank_conn(), reach_count_sqlstr)
 
-    # THEN
-    assert count_reach_rows_num == 94
+#     # THEN
+#     assert reach_rows_num == 94
+
+
+# def test_culture_clear_river_score_CorrectlyEmptysTable(env_dir_setup_cleanup):
+#     pass
