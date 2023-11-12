37f98684f64c6e2868a11da9ac44c0b3dc4e4f8a
Joachim <jschalk@users.noreply.github.com>
2023-11-12 15:12:32 -0800

    #41 clear "subejct" from codebase

--- src/culture/test_culture_bank/test_culture_bank_river_vein.py
+++ src/culture/test_culture_bank/test_culture_bank_river_vein.py
@@ -1,5 +1,5 @@
 # future enhancement. Create tool that populates river_reach_vien:
-# set of all healers that can increase a subject healer's credit_score
+# create list of all healers that can increase a certain healer's credit_score
 # and what currency ranges would help. they control
 
 # def test_culture_clear_river_score_CorrectlyEmptysTable():
