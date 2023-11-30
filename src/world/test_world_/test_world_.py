0b0cabb1c053efe6a9c80bf9da90020cc0f1242f
Joachim <jschalk@users.noreply.github.com>
2023-11-30 15:10:03 -0800

    #41 set_personunit

--- src/world/test_world_/test_world_.py
+++ src/world/test_world_/test_world_.py
@@ -1,6 +1,7 @@
 from src.world.world import WorldUnit, worldunit_shop
 from src.world.examples.world_env_kit import get_test_worlds_dir
-from src.world.person import personunit_shop
+from src.world.person import personunit_shop, painunit_shop
+from pytest import raises as pytest_raises
 
 
 def test_worldunit_exists():
@@ -70,7 +71,7 @@ def test_worldunit__set_person_in_memory_CorrectlySetsPerson():
     assert wx._persons_dir == f"{wx._world_dir}/persons"
 
 
-def test_worldunit__create_person_from_pid_CorrectlySetsPerson():
+def test_worldunit_add_personunit_CorrectlySetsPerson():
     # GIVEN
     dallas_text = "dallas"
     wx = worldunit_shop(mark=dallas_text, worlds_dir=get_test_worlds_dir())
@@ -79,12 +80,67 @@ def test_worldunit__create_person_from_pid_CorrectlySetsPerson():
     luca_person_obj = personunit_shop(pid=luca_text, person_dir=luca_person_dir)
 
     # WHEN
-    wx._create_person_from_pid(luca_text)
+    wx.add_personunit(luca_text)
 
     # THEN
     assert wx._persons_obj[luca_text] != None
     assert wx._persons_obj[luca_text].person_dir == luca_person_dir
     assert wx._persons_obj[luca_text] == luca_person_obj
+
+
+def test_worldunit_add_personunit_RaisesErrorIfPersonExists():
+    # GIVEN
+    dallas_text = "dallas"
+    wx = worldunit_shop(mark=dallas_text, worlds_dir=get_test_worlds_dir())
+    luca_text = "Luca"
+    luca_person_dir = f"{wx._persons_dir}/{luca_text}"
+    luca_person_obj = personunit_shop(pid=luca_text, person_dir=luca_person_dir)
+    wx.add_personunit(luca_text)
+    assert wx._persons_obj[luca_text] != None
+    assert wx._persons_obj[luca_text].person_dir == luca_person_dir
+    assert wx._persons_obj[luca_text] == luca_person_obj
+
+    # WHEN/THEN
+    with pytest_raises(Exception) as excinfo:
+        wx.add_personunit(luca_text)
+    assert str(excinfo.value) == f"add_personunit fail: {luca_text} already exists"
+
+
+def test_worldunit_set_personunit_CorrectlyCreatesObj():
+    # GIVEN
+    dallas_text = "dallas"
+    wx = worldunit_shop(mark=dallas_text, worlds_dir=get_test_worlds_dir())
+    luca_text = "Luca"
+    assert wx._persons_obj.get(luca_text) is None
+
+    # WHEN
+    luca_person_dir = f"{wx._persons_dir}/{luca_text}"
+    luca_person_obj = personunit_shop(pid=luca_text, person_dir=luca_person_dir)
+    wx.set_personunit(luca_person_obj)
+
+    # THEN
+    assert wx._persons_obj.get(luca_text) != None
+    assert wx._persons_obj.get(luca_text).person_dir == luca_person_dir
+    assert wx._persons_obj.get(luca_text) == luca_person_obj
+
+
+def test_worldunit_set_personunit_CorrectlyReplacesObj():
+    # GIVEN
+    dallas_text = "dallas"
+    wx = worldunit_shop(mark=dallas_text, worlds_dir=get_test_worlds_dir())
+    luca_text = "Luca"
+    wx.add_personunit(luca_text)
+    luca_person = wx.get_personunit_from_memory(luca_text)
+    luca_person.set_painunit(painunit_shop("Bob"))
+    assert wx._persons_obj.get(luca_text) != None
+    assert len(wx._persons_obj.get(luca_text)._pains) == 1
+
+    # WHEN
+    luca_person_dir = f"{wx._persons_dir}/{luca_text}"
+    wx.set_personunit(personunit_shop(pid=luca_text, person_dir=luca_person_dir))
+
+    # THEN
+    assert len(wx._persons_obj.get(luca_text)._pains) == 0
 
 
 def test_worldunit_get_personunit_from_memory_CorrectlyReturnsPerson():
@@ -94,7 +150,7 @@ def test_worldunit_get_personunit_from_memory_CorrectlyReturnsPerson():
     luca_text = "Luca"
     luca_person_dir = f"{wx._persons_dir}/{luca_text}"
     luca_person_obj = personunit_shop(pid=luca_text, person_dir=luca_person_dir)
-    wx._create_person_from_pid(luca_text)
+    wx.add_personunit(luca_text)
 
     # WHEN
     luca_gotten_obj = wx.get_personunit_from_memory(luca_text)
--- src/world/world.py
+++ src/world/world.py
@@ -2,6 +2,10 @@ from dataclasses import dataclass
 from src.world.person import PersonID, PersonUnit, personunit_shop
 
 
+class PersonExistsException(Exception):
+    pass
+
+
 class WorldMark(str):  # Created to help track the concept
     pass
 
@@ -14,6 +18,9 @@ class WorldUnit:
     _world_dir: str = None
     _persons_obj: dict[PersonID:PersonUnit] = None
 
+    def get_person_dir(self, person_id):
+        return f"{self._persons_dir}/{person_id}"
+
     def _set_world_dirs(self):
         self._world_dir = f"{self.worlds_dir}/{self.mark}"
         self._persons_dir = f"{self._world_dir}/persons"
@@ -25,10 +32,17 @@ class WorldUnit:
     def _set_person_in_memory(self, personunit: PersonUnit):
         self._persons_obj[personunit.pid] = personunit
 
-    def _create_person_from_pid(self, person_id: PersonID):
-        x_person_dir = f"{self._persons_dir}/{person_id}"
-        x_person_obj = personunit_shop(pid=person_id, person_dir=x_person_dir)
-        self._set_person_in_memory(x_person_obj)
+    def set_personunit(self, personunit: PersonUnit):
+        self._set_person_in_memory(personunit)
+
+    def add_personunit(self, person_id: PersonID):
+        x_personunit = personunit_shop(person_id, self.get_person_dir(person_id))
+        if self._persons_obj.get(x_personunit.pid) is None:
+            self.set_personunit(x_personunit)
+        else:
+            raise PersonExistsException(
+                f"add_personunit fail: {x_personunit.pid} already exists"
+            )
 
     def get_personunit_from_memory(self, person_id: PersonID) -> PersonUnit:
         return self._persons_obj.get(person_id)
