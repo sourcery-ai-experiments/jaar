07c605e07f8ae7d5694eaecebb217c499531ae90
Joachim <jschalk@users.noreply.github.com>
2024-04-24 11:13:26 -0700

    #121 create empty job works

--- src/econ/clerk.py
+++ src/econ/clerk.py
@@ -23,6 +23,10 @@ class InvalidclerkException(Exception):
     pass
 
 
+class RoleAgendaFileException(Exception):
+    pass
+
+
 def get_owner_file_name(x_owner_id: str) -> str:
     return f"{x_owner_id}.json"
 
@@ -51,6 +55,24 @@ def save_file_to_jobs(x_econ_dir: str, x_agenda: AgendaUnit):
     )
 
 
+def get_file_in_guts(x_econ_dir: str, owner_id: PersonID) -> AgendaUnit:
+    gut_file_name = get_owner_file_name(owner_id)
+    gut_dir = get_econ_guts_dir(x_econ_dir)
+    try:
+        gut_file_text = open_file(gut_dir, gut_file_name)
+    except:
+        raise RoleAgendaFileException(
+            f"Role agenda file '{gut_file_name}' does not exist."
+        )
+    return agendaunit_get_from_json(gut_file_text)
+
+
+def get_file_in_jobs(x_econ_dir: str, owner_id: PersonID) -> AgendaUnit:
+    job_file_name = get_owner_file_name(owner_id)
+    job_dir = get_econ_jobs_dir(x_econ_dir)
+    return agendaunit_get_from_json(open_file(job_dir, job_file_name))
+
+
 @dataclass
 class ClerkUnit:
     _clerk_id: ClerkID = None
@@ -58,9 +80,10 @@ class ClerkUnit:
     _guts_dir: str = None
     _jobs_dir: str = None
     _role_file_path: str = None
-    _road_delimiter: str = None
+    # _road_delimiter: str = None
     _role: AgendaUnit = None
-    _roll: dict[PartyID:PartyUnit] = None
+    _job: AgendaUnit = None
+    _roll: list[(PartyID, PartyUnit)] = None
 
     def set_clerk_id(self, x_clerk_id: PersonID):
         self._clerk_id = x_clerk_id
@@ -68,8 +91,27 @@ class ClerkUnit:
     def set_econ_dir(self, x_econ_dir: str):
         self._econ_dir = x_econ_dir
 
-    def set_road_delimiter(self, x_road_delimiter: str):
-        self._road_delimiter = x_road_delimiter
+    def _set_role(self):
+        self._role = get_file_in_guts(self._econ_dir, self._clerk_id)
+
+    def _set_roll(self):
+        for x_partyunit in self._role._partys.values():
+            self._roll.append((x_partyunit.party_id, x_partyunit.debtor_weight))
+
+    def _set_empty_job(self):
+        self._job = agendaunit_shop(
+            self._role._owner_id,
+            self._role._world_id,
+            _road_delimiter=self._role._road_delimiter,
+        )
+        self._job._partys = self._role._partys
+        self._job._groups = self._role._groups
+        self._job._planck = self._role._planck
+        self._job.set_money_desc(self._role._money_desc)
+        if self._role._party_creditor_pool != None:
+            self._job.set_party_creditor_pool(self._role._party_creditor_pool)
+        if self._role._party_debtor_pool != None:
+            self._job.set_party_debtor_pool(self._role._party_debtor_pool)
 
     # def refresh_depot_agendas(self):
     #     for party_x in self._role._partys.values():
@@ -110,11 +152,6 @@ class ClerkUnit:
     #     if self._role is None:
     #         self._role = self.open_role_file()
     #     return self._role
-
-    # def set_role(self, x_agenda: AgendaUnit = None):
-    #     if x_agenda != None:
-    #         self._role = x_agenda
-    #         self.save_role_agenda(self._role)
 
     # def set_role_if_empty(self):
     #     # if self._role is None:
@@ -280,10 +317,17 @@ class ClerkUnit:
     #     self.save_agenda_to_jobs(self.get_remelded_output_agenda())
 
 
-def clerkunit_shop(clerk_id: PersonID, env_dir: str) -> ClerkUnit:
+def clerkunit_shop(
+    clerk_id: PersonID, env_dir: str, create_job: bool = True
+) -> ClerkUnit:
     x_clerk = ClerkUnit()
+    x_clerk._roll = []
     x_clerk.set_econ_dir(x_econ_dir=env_dir)
     x_clerk.set_clerk_id(x_clerk_id=clerk_id)
-    # x_clerk. _road_delimiter=role._road_delimiter,
     x_clerk._set_clerkunit_dirs()
+    if create_job:
+        x_clerk._set_role()
+        x_clerk._set_roll()
+        x_clerk._set_empty_job()
+    # x_clerk. _road_delimiter=role._road_delimiter,
     return x_clerk
--- src/econ/test_clerk/test_clerk_.py
+++ src/econ/test_clerk/test_clerk_.py
@@ -1,7 +1,16 @@
 from src._road.road import default_road_delimiter_if_none
+from src.agenda.party import partylink_shop
+from src.agenda.group import groupunit_shop
+from src.agenda.idea import ideaunit_shop
 from src.agenda.agenda import agendaunit_shop
 from src.instrument.file import delete_dir
-from src.econ.clerk import clerkunit_shop, ClerkUnit
+from src.econ.clerk import (
+    clerkunit_shop,
+    ClerkUnit,
+    save_file_to_guts,
+    save_file_to_jobs,
+    get_owner_file_name,
+)
 from src.econ.econ import get_econ_jobs_dir, get_econ_guts_dir
 from src.econ.examples.econ_env_kit import (
     env_dir_setup_cleanup,
@@ -9,6 +18,7 @@ from src.econ.examples.econ_env_kit import (
     get_temp_env_world_id,
 )
 from os import path as os_path
+from pytest import raises as pytest_raises
 
 
 def test_ClerkUnit_exists():
@@ -22,8 +32,9 @@ def test_ClerkUnit_exists():
     assert x_clerk._guts_dir is None
     assert x_clerk._jobs_dir is None
     assert x_clerk._role_file_path is None
-    assert x_clerk._road_delimiter is None
+    # assert x_clerk._road_delimiter is None
     assert x_clerk._role is None
+    assert x_clerk._job is None
     assert x_clerk._roll is None
 
 
@@ -53,17 +64,17 @@ def test_ClerkUnit_set_econ_dir_SetsArribute():
     assert x_clerk._econ_dir == x_dir
 
 
-def test_ClerkUnit_set_road_delimiter_SetsArribute():
-    # GIVEN
-    x_clerk = ClerkUnit()
-    assert x_clerk._road_delimiter is None
+# def test_ClerkUnit_set_road_delimiter_SetsArribute():
+#     # GIVEN
+#     x_clerk = ClerkUnit()
+#     assert x_clerk._road_delimiter is None
 
-    # WHEN
-    slash_text = "/"
-    x_clerk.set_road_delimiter(slash_text)
+#     # WHEN
+#     slash_text = "/"
+#     x_clerk.set_road_delimiter(slash_text)
 
-    # THEN
-    assert x_clerk._road_delimiter == slash_text
+#     # THEN
+#     assert x_clerk._road_delimiter == slash_text
 
 
 def test_ClerkUnit_set_clerkunit_dirs_SetsArributes():
@@ -82,18 +93,133 @@ def test_ClerkUnit_set_clerkunit_dirs_SetsArributes():
     assert x_clerk._jobs_dir == get_econ_jobs_dir(x_dir)
 
 
-def test_clerkunit_shop_CorrectlyLoads(env_dir_setup_cleanup):
+def test_clerkunit_shop_DoesNotCreateJob(env_dir_setup_cleanup):
     # GIVEN
     yao_text = "Yao"
 
     # WHEN
-    x_clerk = clerkunit_shop(yao_text, get_test_econ_dir())
+    yao_clerk = clerkunit_shop(yao_text, get_test_econ_dir(), create_job=False)
 
     # GIVEN
-    assert x_clerk._clerk_id != None
-    assert x_clerk._clerk_id == yao_text
-    assert x_clerk._econ_dir == get_test_econ_dir()
-    assert x_clerk._role is None
+    assert yao_clerk._clerk_id != None
+    assert yao_clerk._clerk_id == yao_text
+    assert yao_clerk._econ_dir == get_test_econ_dir()
+    assert yao_clerk._role is None
+    assert yao_clerk._roll == []
+    assert yao_clerk._job is None
+
+
+def test_ClerkUnit_set_role_SetsArributesWhenFileExists(env_dir_setup_cleanup):
+    # GIVEN
+    yao_text = "Yao"
+    yao_agenda = agendaunit_shop(yao_text)
+    save_file_to_guts(get_test_econ_dir(), yao_agenda)
+    yao_clerk = clerkunit_shop(yao_text, get_test_econ_dir(), create_job=False)
+    assert yao_clerk._role is None
+
+    # WHEN
+    yao_clerk._set_role()
+
+    # THEN
+    assert yao_clerk._role.get_dict() == yao_agenda.get_dict()
+
+
+def test_ClerkUnit_set_role_SetsArributesWhenFileDoesNotExist(env_dir_setup_cleanup):
+    # GIVEN
+    yao_text = "Yao"
+    yao_clerk = clerkunit_shop(yao_text, get_test_econ_dir(), create_job=False)
+    assert yao_clerk._role is None
+
+    # WHEN / THEN
+    with pytest_raises(Exception) as excinfo:
+        yao_clerk._set_role()
+    assert (
+        str(excinfo.value)
+        == f"Role agenda file '{get_owner_file_name(yao_text)}' does not exist."
+    )
+
+
+def test_ClerkUnit_set_roll_SetsArribute(env_dir_setup_cleanup):
+    # GIVEN
+    yao_text = "Yao"
+    yao_agenda = agendaunit_shop(yao_text)
+    zia_text = "Zia"
+    zia_creditor_weight = 47
+    zia_debtor_weight = 41
+    yao_agenda.add_partyunit(zia_text, zia_creditor_weight, zia_debtor_weight)
+    yao_agenda.set_agenda_metrics()
+    save_file_to_guts(get_test_econ_dir(), yao_agenda)
+    yao_clerk = clerkunit_shop(yao_text, get_test_econ_dir(), create_job=False)
+    yao_clerk._set_role()
+    assert yao_clerk._roll == []
+
+    # WHEN
+    yao_clerk._set_roll()
+
+    # THEN
+    assert yao_clerk._roll != [(zia_text, zia_creditor_weight)]
+    assert yao_clerk._roll == [(zia_text, zia_debtor_weight)]
+
+
+def test_ClerkUnit_set_empty_job_SetsArributeEmptyAgendaUnit(env_dir_setup_cleanup):
+    # GIVEN
+    yao_text = "Yao"
+    slash_text = "/"
+    yao_agenda = agendaunit_shop(yao_text, _road_delimiter=slash_text)
+    yao_agenda.add_l1_idea(ideaunit_shop("Texas"))
+    zia_text = "Zia"
+    zia_creditor_weight = 47
+    zia_debtor_weight = 41
+    zia_creditor_pool = 87
+    zia_debtor_pool = 81
+    yao_agenda.add_partyunit(zia_text, zia_creditor_weight, zia_debtor_weight)
+    swim_group = groupunit_shop(f"{slash_text}swimmers", _road_delimiter=slash_text)
+    swim_group.set_partylink(partylink_shop(zia_text))
+    yao_agenda.set_groupunit(swim_group)
+    yao_agenda.set_party_creditor_pool(zia_creditor_pool, True)
+    yao_agenda.set_party_debtor_pool(zia_debtor_pool, True)
+    save_file_to_guts(get_test_econ_dir(), yao_agenda)
+    yao_clerk = clerkunit_shop(yao_text, get_test_econ_dir(), create_job=False)
+    yao_clerk._set_role()
+    assert yao_clerk._roll == []
+
+    # WHEN
+    yao_clerk._set_empty_job()
+
+    # THEN
+    assert yao_clerk._job._owner_id == yao_agenda._owner_id
+    assert yao_clerk._job._world_id == yao_agenda._world_id
+    assert yao_clerk._job._last_gift_id == yao_agenda._last_gift_id
+    assert yao_clerk._job.get_partys_dict() == yao_agenda.get_partys_dict()
+    assert yao_clerk._job.get_groupunits_dict() == yao_agenda.get_groupunits_dict()
+    assert yao_clerk._job._road_delimiter == yao_agenda._road_delimiter
+    assert yao_clerk._job._planck == yao_agenda._planck
+    assert yao_clerk._job._money_desc == yao_agenda._money_desc
+    assert yao_clerk._job._party_creditor_pool == yao_agenda._party_creditor_pool
+    assert yao_clerk._job._party_debtor_pool == yao_agenda._party_debtor_pool
+    yao_clerk._job.set_agenda_metrics()
+    assert len(yao_clerk._job._idea_dict) != len(yao_agenda._idea_dict)
+    assert len(yao_clerk._job._idea_dict) == 1
+
+
+def test_clerkunit_shop_CreatesEmptyJob(env_dir_setup_cleanup):
+    # GIVEN
+    yao_text = "Yao"
+    yao_agenda = agendaunit_shop(yao_text)
+    yao_agenda.set_agenda_metrics()
+    save_file_to_guts(get_test_econ_dir(), yao_agenda)
+
+    # WHEN
+    yao_clerk = clerkunit_shop(yao_text, get_test_econ_dir())
+
+    # GIVEN
+    assert yao_clerk._clerk_id != None
+    assert yao_clerk._clerk_id == yao_text
+    assert yao_clerk._econ_dir == get_test_econ_dir()
+    assert yao_clerk._role._idea_dict == yao_agenda._idea_dict
+    assert yao_clerk._role == yao_agenda
+    assert yao_clerk._roll == []
+    assert yao_clerk._job.get_dict() == yao_agenda.get_dict()
 
 
 # # def test_clerkunit_auto_output_job_to_jobs_SavesAgendaTojobsDirWhenTrue(
