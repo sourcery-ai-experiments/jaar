0db1628e2160841291d49205bcde62dae4d0eea6
Joachim <jschalk@users.noreply.github.com>
2024-06-25 18:29:59 -0700

    #221 create BeliefLink class

--- src/_world/belief.py
+++ src/_world/belief.py
@@ -224,6 +224,29 @@ def beliefunit_shop(
 
 
 @dataclass
+class BeliefLink(BeliefCore):
+    credor_weight: float = 1.0
+    debtor_weight: float = 1.0
+
+    def get_dict(self) -> dict[str:str]:
+        return {
+            "belief_id": self.belief_id,
+            "credor_weight": self.credor_weight,
+            "debtor_weight": self.debtor_weight,
+        }
+
+
+def belieflink_shop(
+    belief_id: BeliefID, credor_weight: float = None, debtor_weight: float = None
+) -> BeliefLink:
+    credor_weight = get_1_if_None(credor_weight)
+    debtor_weight = get_1_if_None(debtor_weight)
+    return BeliefLink(
+        belief_id=belief_id, credor_weight=credor_weight, debtor_weight=debtor_weight
+    )
+
+
+@dataclass
 class BalanceLink(BeliefCore):
     credor_weight: float = 1.0
     debtor_weight: float = 1.0
--- src/_world/test_/test_belief.py
+++ src/_world/test_/test_belief.py
@@ -5,6 +5,8 @@ from src._world.belief import (
     BeliefUnit,
     beliefunit_shop,
     BeliefID,
+    BeliefLink,
+    belieflink_shop,
     BalanceLink,
     balancelink_shop,
     balancelinks_get_from_json,
@@ -562,6 +564,62 @@ def test_balancelink_shop_ReturnsCorrectObj():
     # THEN
     assert bikers_balancelink.credor_weight == bikers_credor_weight
     assert bikers_balancelink.debtor_weight == bikers_debtor_weight
+
+
+def test_BeliefLink_exists():
+    # GIVEN
+    bikers_belief_id = BeliefID("bikers")
+
+    # WHEN
+    bikers_belieflink = BeliefLink(belief_id=bikers_belief_id)
+
+    # THEN
+    assert bikers_belieflink.belief_id == bikers_belief_id
+    assert bikers_belieflink.credor_weight == 1.0
+    assert bikers_belieflink.debtor_weight == 1.0
+
+
+def test_belieflink_shop_ReturnsCorrectObj():
+    # GIVEN
+    bikers_belief_id = BeliefID("bikers")
+    bikers_credor_weight = 3.0
+    bikers_debtor_weight = 5.0
+
+    # WHEN
+    bikers_belieflink = belieflink_shop(
+        belief_id=bikers_belief_id,
+        credor_weight=bikers_credor_weight,
+        debtor_weight=bikers_debtor_weight,
+    )
+
+    # THEN
+    assert bikers_belieflink.credor_weight == bikers_credor_weight
+    assert bikers_belieflink.debtor_weight == bikers_debtor_weight
+
+
+def test_BeliefLink_get_dict_ReturnsDictWithNecessaryDataForJSON():
+    # GIVEN
+    bikers_belief_id = BeliefID("bikers")
+    bikers_credor_weight = 3.0
+    bikers_debtor_weight = 5.0
+    bikers_link = belieflink_shop(
+        belief_id=bikers_belief_id,
+        credor_weight=bikers_credor_weight,
+        debtor_weight=bikers_debtor_weight,
+    )
+
+    print(f"{bikers_link}")
+
+    # WHEN
+    biker_dict = bikers_link.get_dict()
+
+    # THEN
+    assert biker_dict != None
+    assert biker_dict == {
+        "belief_id": bikers_link.belief_id,
+        "credor_weight": bikers_link.credor_weight,
+        "debtor_weight": bikers_link.debtor_weight,
+    }
 
 
 def test_BalanceHeir_set_world_importance_CorrectlySetsAttr():
